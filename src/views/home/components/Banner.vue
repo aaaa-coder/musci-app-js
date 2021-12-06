82edc286cc7cb740b47668823a241e4523dd8e7d
wp-product <tchp@wpkj.net>
2021-12-06 19:34:32 +0800

    封装banner组件，但有一个无法修改pagination颜色的bug

---
+++ src/views/home/components/Banner.vue
@@ -0,0 +1,87 @@
+<template>
+  <swiper
+    :autoplay="swiper_options.autoplay"
+    :loop="swiper_options.loop"
+    :speed="swiper_options.speed"
+    :pagination="swiper_options.pagination"
+    :spaceBetween="swiper_options.spaceBetween"
+    :coverflowEffect="swiper_options.coverflowEffect"
+    :centeredSlides="swiper_options.centeredSlides"
+    :slidesPerView="swiper_options.slidesPerView"
+    class="swiper"
+    effect="coverflow"
+  >
+    <swiper-slide
+      ><img
+        class="my_swiper_imgs"
+        src="../../../assets/images/home/building-6822998__480.jpg"
+        alt=""
+    /></swiper-slide>
+    <swiper-slide
+      ><img
+        class="my_swiper_imgs"
+        src="../../../assets/images/home/coffee-6487099__340.jpg"
+        alt=""
+    /></swiper-slide>
+    <swiper-slide
+      ><img
+        class="my_swiper_imgs"
+        src="../../../assets/images/home/rhinoceros-6799726__340.jpg"
+        alt=""
+    /></swiper-slide>
+  </swiper>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import SwiperCore, {
+  Autoplay,
+  Pagination,
+  EffectCoverflow,
+  Navigation,
+} from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/navigation/navigation.scss";
+SwiperCore.use([Autoplay, Pagination, EffectCoverflow, Navigation]);
+
+let swiper_options = reactive({
+  autoplay: {
+    disableOnInteraction: true, // 鼠标滑动后继续自动播放
+    delay: 2000, //4秒切换一次
+  },
+  speed: 500, //切换过渡速度
+  loop: true,
+  slidesPerView: "auto", //解决最后一张切换到第一张中间的空白
+  centeredSlides: true, //设置slide居中
+  spaceBetween: 20,
+  coverflowEffect: {
+    // rotate: 0, //slide做3d旋转时Y轴的旋转角度。默认50。
+    stretch: 50, //每个slide之间的拉伸值（距离），越大slide靠得越紧。 默认0。
+    depth: 100, //slide的位置深度。值越大z轴距离越远，看起来越小。 默认100。
+    modifier: 1, //depth和rotate和stretch的倍率，相当于            depth*modifier、rotate*modifier、stretch*modifier，值越大这三个参数的效果越明显。默认1。
+    // slideShadows: false, //开启slide阴影。默认 true。
+  },
+  pagination: {
+    clickable: true,
+    // bulletActiveClass: "my-bullet-active",
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+// :deep(.swiper-pagination-bullet-active) {
+//   color: #bf0b0b !important;
+//   opacity: 1;
+// }
+// FIXED：无法修改pagination样式
+:deep(.swiper-pagination-bullet-active) {
+  color: red;
+  opacity: 1;
+}
+// .my-bullet-active {
+//   color: #bf0b0b !important;
+//   opacity: 1;
+// }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -12,37 +12,7 @@
     </div>
 
     <div class="banner">
-      <swiper
-        :autoplay="swiper_options.autoplay"
-        :loop="swiper_options.loop"
-        :speed="swiper_options.speed"
-        :pagination="swiper_options.pagination"
-        :spaceBetween="swiper_options.spaceBetween"
-        :coverflowEffect="swiper_options.coverflowEffect"
-        :centeredSlides="swiper_options.centeredSlides"
-        :slidesPerView="swiper_options.slidesPerView"
-        class="swiper"
-        effect="coverflow"
-      >
-        <swiper-slide
-          ><img
-            class="my_swiper_imgs"
-            src="../../assets/images/home/building-6822998__480.jpg"
-            alt=""
-        /></swiper-slide>
-        <swiper-slide
-          ><img
-            class="my_swiper_imgs"
-            src="../../assets/images/home/coffee-6487099__340.jpg"
-            alt=""
-        /></swiper-slide>
-        <swiper-slide
-          ><img
-            class="my_swiper_imgs"
-            src="../../assets/images/home/rhinoceros-6799726__340.jpg"
-            alt=""
-        /></swiper-slide>
-      </swiper>
+      <Banner />
     </div>
   </div>
 </template>
@@ -50,39 +20,8 @@
 <script setup>
 import { getBannerList } from "@/apis/index";
 import { onMounted, defineAsyncComponent, reactive } from "vue";
-import SwiperCore, {
-  Autoplay,
-  Pagination,
-  EffectCoverflow,
-  Navigation,
-} from "swiper";
-import { Swiper, SwiperSlide } from "swiper/vue";
-import "swiper/swiper.min.css";
-import "swiper/components/pagination/pagination.scss";
-import "swiper/components/navigation/navigation.scss";
-SwiperCore.use([Autoplay, Pagination, EffectCoverflow, Navigation]);
+import Banner from "./components/Banner.vue";
 
-let swiper_options = reactive({
-  autoplay: {
-    disableOnInteraction: false, // 鼠标滑动后继续自动播放
-    delay: 4000, //4秒切换一次
-  },
-  speed: 500, //切换过渡速度
-  loop: true,
-  slidesPerView: "auto", //解决最后一张切换到第一张中间的空白
-  centeredSlides: true, //设置slide居中
-  spaceBetween: 20,
-  coverflowEffect: {
-    // rotate: 0, //slide做3d旋转时Y轴的旋转角度。默认50。
-    stretch: 50, //每个slide之间的拉伸值（距离），越大slide靠得越紧。 默认0。
-    depth: 100, //slide的位置深度。值越大z轴距离越远，看起来越小。 默认100。
-    modifier: 1, //depth和rotate和stretch的倍率，相当于            depth*modifier、rotate*modifier、stretch*modifier，值越大这三个参数的效果越明显。默认1。
-    // slideShadows: false, //开启slide阴影。默认 true。
-  },
-  pagination: {
-    clickable: true,
-  },
-});
 const AsyncHeader = defineAsyncComponent(() =>
   import("../../components/Header/index.vue")
 );
