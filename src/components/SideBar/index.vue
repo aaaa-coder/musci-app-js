fc5372eec2e480aef3f0d926a329f2d0c21eec8e
wp-product <tchp@wpkj.net>
2021-12-16 20:01:15 +0800

    侧边栏会员中心

--- src/components/SideBar/index.vue
+++ src/components/SideBar/index.vue
@@ -15,7 +15,13 @@
           <h3 class="left_title">开通黑胶VIP</h3>
           <div class="left_content">立享超21项专属特权&gt;</div>
         </div>
+
         <div class="header_right">会员中心</div>
+      </div>
+
+      <div class="share_wrapper">
+        <span class="share_text"> 受邀专享，黑胶首月仅3.8元！ </span>
+        <i class="iconfont"></i>
       </div>
     </div>
   </div>
@@ -47,17 +53,31 @@
 
   .vip_wrapper {
     padding: 20 / 750 * 100vw;
-    height: 200 / 750 * 100vw;
+    height: 170 / 750 * 100vw;
     background-color: #353535;
     border-radius: 30 / 750 * 100vw;
+    color: #888;
     .vip_header {
       display: flex;
+      align-items: center;
+      margin-bottom: 30 / 750 * 100vw;
       .header_left {
         flex: 1;
         .left_title {
           color: #d8d1cd;
+          margin-bottom: 10 / 750 * 100vw;
         }
       }
+      .header_right {
+        padding: 10 / 750 * 100vw 20 / 750 * 100vw;
+        border: 1px solid #ccc;
+        border-radius: 50 / 750 * 100vw;
+      }
+    }
+
+    .share_wrapper {
+      padding-top: 30 / 750 * 100vw;
+      border-top: 1px solid #ccc;
     }
   }
 }
